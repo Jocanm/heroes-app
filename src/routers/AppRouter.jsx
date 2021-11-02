e398f10ed16bf9fabd3aee7cc7641d20f9ee74de
Jose Angarita <amjose09@gmail.com>
2021-11-02 15:34:59 -0500

    Rutas creadas como el profesor listo

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import PrivateLayout from '../layout/PrivateLayout';
+import DcScreen from '../pages/DcScreen';
+import HeroesScreen from '../pages/HeroesScreen'
 import LoginScreen from '../pages/LoginScreen';
-import DashboardRoutes from './DashboardRoutes';
+import MarvelScreen from '../pages/MarvelScreen';
 
 const AppRouter = () => {
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
-                    <Route exact path="/" component={ DashboardRoutes } />
+
+                    <Route path={["/login"]}>
+                        <Switch>
+
+                            <Route path="/login" component={LoginScreen} />
+
+                        </Switch>
+                    </Route>
+
+                    <Route path={["/marvel", "/heroe/:heroeId", "/dc", "/"]} >
+                        <PrivateLayout>
+
+                            <Switch>
+                                <Route path="/marvel" component={MarvelScreen} />
+                                <Route path="/heroe/:heroeId" component={HeroesScreen} />
+                                <Route path="/dc" component={DcScreen} />
+                                <Redirect to="/marvel" />
+                            </Switch>
+
+                        </PrivateLayout>
+                    </Route>
                 </Switch>
             </div>
         </Router>
     )
 }
+// {/* <Switch>
+//     <Route exact path="/login" component={ LoginScreen } />
+//     <Route exact path="/" component={ DashboardRoutes } />
+// </Switch> */}
 
 export default AppRouter
