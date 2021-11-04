d874fcd51602803b4d68c444eb223c815b634c5b
Jose Angarita <amjose09@gmail.com>
2021-11-03 19:35:32 -0500

    Hero information - responsive fixed

--- src/pages/HeroesScreen.jsx
+++ src/pages/HeroesScreen.jsx
@@ -24,15 +24,15 @@ const HeroesScreen = ({history}) => {
     }
 
     return (
-        <div className="p-10 pt-14 flex">
-            <div>
+        <div className="p-10 pt-14 pl-20 flex flex-col md:flex-row">
+            <div className="mb-7">
                 <img
                     src={`../assets/heroes/${id}.jpg`}
                     alt={superhero}
                     className="heroes-screen animate__animated animate__zoomIn"
                 />
             </div>
-            <div className="pl-5 animate__animated animate__fadeIn">
+            <div className="md:pl-5 animate__animated animate__fadeIn">
                 <h2 className="text-4xl font-bold mb-4">{superhero}</h2>
 
                 <ul>
@@ -40,7 +40,7 @@ const HeroesScreen = ({history}) => {
                     <li><b>Publisher:</b> {publisher}</li>
                     <li><b>First appearance:</b> {first_appearance}</li>
                 </ul>
-                <hr className="mt-3 w-96" />
+                <hr className="mt-3 w-96 hidden md:block" />
 
                 <h2 className="mt-4 text-2xl font-bold">
                     Characters
